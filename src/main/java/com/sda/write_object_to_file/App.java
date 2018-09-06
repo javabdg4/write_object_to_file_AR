b160a3eca5a9bb90f36e2363742c3a3a4e04892a
Anna Rozycka <annjoe@vp.pl>
2018-09-06 20:38:36 +0200

    write and read files using jackson plugin

--- src/main/java/com/sda/write_object_to_file/App.java
+++ src/main/java/com/sda/write_object_to_file/App.java
@@ -1,13 +1,50 @@
 package com.sda.write_object_to_file;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<Weather> weathers = new ArrayList<>();
+
+        weathers.add(new Weather("Poznan", "www", 24.00, 25.00, "cloudy", 34.35, 12.56));
+        weathers.add(new Weather("Wroclaw", "www", 13.00, 10.00, "sunny", 34.54, 13.90));
+        weathers.add(new Weather("Bydgoszcz", "www", 20.00, 21.00, "rainy", 52.00, 34.00));
+        weathers.add(new Weather("Gdansk", "www", 7.00, 7.00, "cloudy", 23.00, 34.89));
+        weathers.add(new Weather("Warszawa", "www", 15.00, 12.00, "windy", 40.00, 32.00));
+
+        Weather weather = new Weather(
+                "Torun",
+                "www",
+                40.00,
+                60.00,
+                "sunny",
+                54.54,
+                21.23
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        File filename = new File("weather.json");
+        try {
+            objectMapper.writeValue(filename, weathers);
+           // objectMapper.writeValue(filename, weather);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Weather[] readWeather = objectMapper.readValue(filename,Weather[].class);
+
+            for (Weather w : readWeather){
+                System.out.println(w.getCity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/sda/write_object_to_file/Weather.java
@@ -0,0 +1,81 @@
+package com.sda.write_object_to_file;
+
+public class Weather {
+
+    private String city;
+    private String iconUrl;
+    private double temperature;
+    private double feelsLikeC;
+    private String conditionText;
+    private double lat;
+    private double lon;
+
+    public Weather() {
+    }
+
+    public Weather(String city, String iconUrl, double temperature, double feelsLikeC, String conditionText, double lat, double lon) {
+        this.city = city;
+        this.iconUrl = iconUrl;
+        this.temperature = temperature;
+        this.feelsLikeC = feelsLikeC;
+        this.conditionText = conditionText;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getFeelsLikeC() {
+        return feelsLikeC;
+    }
+
+    public void setFeelsLikeC(double feelsLikeC) {
+        this.feelsLikeC = feelsLikeC;
+    }
+
+    public String getConditionText() {
+        return conditionText;
+    }
+
+    public void setConditionText(String conditionText) {
+        this.conditionText = conditionText;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
